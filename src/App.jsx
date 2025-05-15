3bde928ee45db45ae23e1aa390aa1da58cab3cf7
KauzmoD <lance.deherrera@gmail.com>
2025-05-14 19:31:15 -0600

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import DailyDashboard from "./DailyDashboard";
+
+function App() {
+  return (
+    <div className="App">
+      <DailyDashboard />
+    </div>
+  );
+}
+
+export default App;
